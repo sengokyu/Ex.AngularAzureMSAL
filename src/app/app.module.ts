f236d1ac6517f118f3f854d6d8b353ea8870c4d1
sengokyu <sengokyu+gh@gmail.com>
2021-02-18 10:46:10 +0900

    Update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {
+  MsalAngularConfiguration,
+  MsalInterceptor,
+  MsalModule,
+} from '@azure/msal-angular';
+import { Configuration } from 'msal';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const msalConfig: Configuration = {
+  auth: {
+    clientId: environment.msalClientId,
+    authority: environment.msalAuthority,
+    redirectUri: environment.msalRedirectUri,
+  },
+  cache: {
+    cacheLocation: 'localStorage',
+  },
+};
+const msalAngularConfig: MsalAngularConfiguration = {
+  popUp: false,
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot(msalConfig, msalAngularConfig),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
